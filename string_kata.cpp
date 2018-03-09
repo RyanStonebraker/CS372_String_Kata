87ae03ae7f628f6dd1e7236f9b0205886a949183
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-03-08 15:05:49 -0900

    Adds different delimination Passes the first 7 tests and can easily be delimited by unique values.

--- string_kata.cpp
+++ string_kata.cpp
@@ -6,14 +6,27 @@
 
 long stringSum (const std::string & inputStr) {
   std::vector <long> numbers;
-  std::istringstream strParse(inputStr);
-
   unsigned total = 0;
+
+  std::string argumentBuffer = inputStr;
+  std::string delimiter = ",";
+  if (argumentBuffer.find(delimiter)) {
+    auto foundDelimiter = argumentBuffer.find(delimiter);
+    while (foundDelimiter != std::string::npos) {
+      argumentBuffer.replace(foundDelimiter, delimiter.size(), " ");
+      foundDelimiter = argumentBuffer.find(delimiter);
+    }
+  }
+
+  std::istringstream strParse(argumentBuffer);
 
   long temp = 0;
   while (strParse >> temp) {
+    if (temp > 1000)
+      continue;
     if (temp < 0)
       throw(std::out_of_range("stringSum(const std::string &) : String number must be >= 0."));
+
     numbers.push_back(temp);
   }
 
--- tests/testcases.cpp
+++ tests/testcases.cpp
@@ -61,3 +61,37 @@ TEST_CASE ("Single Number Tests.") {
     }
   }
 }
+
+TEST_CASE("Comma Delimited Numbers") {
+  SECTION ("Two Numbers") {
+    REQUIRE(stringSum("1,1") == 2);
+    REQUIRE(stringSum("9,8") == 17);
+    REQUIRE(stringSum("5,5") == 10);
+    REQUIRE(stringSum("125,54") == 125+54);
+    REQUIRE(stringSum("123445,55314") == 123445+55314);
+    REQUIRE(stringSum("999456,6341") == 999456+6341);
+  }
+  SECTION ("Multiple Numbers") {
+    REQUIRE(stringSum("1,1,1") == 3);
+    REQUIRE(stringSum("9,8,241") == 9 + 8 + 241);
+    REQUIRE(stringSum("2313, 5113, 4621, 6621,    431,2") == 2313 + 5113 + 4621 + 6621 + 431 + 2);
+    REQUIRE(stringSum("2, 523,  5") == 2 + 523 + 5);
+  }
+}
+
+TEST_CASE("Newline Delimited Numbers") {
+  SECTION ("Multiple Numbers") {
+    REQUIRE(stringSum("1\n1") == 2);
+    REQUIRE(stringSum("1\n0") == 1);
+    REQUIRE(stringSum("131\n4141\n0\n896") == 131 + 4141 + 0 + 896);
+    REQUIRE(stringSum("16211\n415141\n10\n896\n4") == 16211 + 415141 + 10 + 896 + 4);
+  }
+}
+
+TEST_CASE("Newline and Comma Delimited Numbers") {
+  SECTION ("Multiple Numbers") {
+    REQUIRE(stringSum("1\n0,3") == 4);
+    REQUIRE(stringSum("51\n31,9") == 51 + 31 + 9);
+    REQUIRE(stringSum("124,41, 54\n78\n2") == 124 + 41 + 54 + 78 + 2);
+  }
+}
