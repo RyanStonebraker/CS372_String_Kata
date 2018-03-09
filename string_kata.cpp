10d18ac0e2ce4f84ceea41cdfb3d3cb4afcbd46d
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-03-09 01:11:02 -0900

    Passes all 10 tests for the Kata Adds tests for unique delimiting for single or multiple characters if the first line starts with '//' and passes the tests

--- string_kata.cpp
+++ string_kata.cpp
@@ -4,17 +4,65 @@
 #include <vector>
 #include <stdexcept>
 
+#include <iostream>
+// #include <string>
+// int main () {
+//   std::string test = "_[abc][123]_";
+//   // std::cout << test.substr(test.find("["), test.find("]"));
+//   test.replace(1, 5, "");
+//   std::cout << test << std::endl;
+// }
+
 long stringSum (const std::string & inputStr) {
   std::vector <long> numbers;
   unsigned total = 0;
 
+  std::vector <std::string> delimiters {","};
+
   std::string argumentBuffer = inputStr;
-  std::string delimiter = ",";
-  if (argumentBuffer.find(delimiter)) {
-    auto foundDelimiter = argumentBuffer.find(delimiter);
-    while (foundDelimiter != std::string::npos) {
-      argumentBuffer.replace(foundDelimiter, delimiter.size(), " ");
-      foundDelimiter = argumentBuffer.find(delimiter);
+
+  if (inputStr.substr(0, 2) == "//") {
+    std::istringstream readDelimiters(inputStr);
+    std::string delimiterLine = "";
+    getline(readDelimiters, delimiterLine);
+
+    delimiterLine.replace(0,2, "");
+
+    auto startBracket = delimiterLine.find_first_of("[");
+    if (startBracket != std::string::npos) {
+      while (true) {
+        auto endBracket = delimiterLine.find_first_of("]");
+        if (startBracket >= endBracket)
+          break;
+
+        if (endBracket != std::string::npos) {
+          if (endBracket > startBracket + 1)
+            delimiters.push_back(delimiterLine.substr(startBracket+1, endBracket-startBracket-1));
+
+          delimiterLine.replace(startBracket, endBracket-startBracket+1, "");
+        }
+        startBracket = delimiterLine.find_first_of("[");
+        if (startBracket == std::string::npos || endBracket == std::string::npos)
+          break;
+      }
+    }
+
+    std::string tempDelimiter = "";
+    std::istringstream delimiterReader (delimiterLine);
+    while (delimiterReader >> tempDelimiter) {
+      delimiters.push_back(tempDelimiter);
+    }
+
+    argumentBuffer = argumentBuffer = inputStr.substr(inputStr.find_first_of("\n") + 1, inputStr.size() - inputStr.find_first_of("\n"));
+  }
+
+  for (auto & delimiter : delimiters) {
+    if (argumentBuffer.find(delimiter) != std::string::npos) {
+      auto foundDelimiter = argumentBuffer.find(delimiter);
+      while (foundDelimiter != std::string::npos) {
+        argumentBuffer.replace(foundDelimiter, delimiter.size(), " ");
+        foundDelimiter = argumentBuffer.find(delimiter);
+      }
     }
   }
 
@@ -22,8 +70,10 @@ long stringSum (const std::string & inputStr) {
 
   long temp = 0;
   while (strParse >> temp) {
+
     if (temp > 1000)
       continue;
+
     if (temp < 0)
       throw(std::out_of_range("stringSum(const std::string &) : String number must be >= 0."));
 
--- tests/testcases.cpp
+++ tests/testcases.cpp
@@ -23,12 +23,12 @@ TEST_CASE ("Single Number Tests.") {
     REQUIRE (stringSum("99") == 99);
   }
   SECTION ("Many Digits") {
-    REQUIRE (stringSum("12131231") == 12131231);
-    REQUIRE (stringSum("5252332") == 5252332);
-    REQUIRE (stringSum("1000000000") == 1000000000);
-    REQUIRE (stringSum("909012331") == 909012331);
-    REQUIRE (stringSum("99857") == 99857);
-    REQUIRE (stringSum("1245299") == 1245299);
+    REQUIRE (stringSum("121") == 121);
+    REQUIRE (stringSum("5252332") == 0);
+    REQUIRE (stringSum("1000") == 1000);
+    REQUIRE (stringSum("999") == 999);
+    REQUIRE (stringSum("99857") == 0);
+    REQUIRE (stringSum("1245299") == 0);
   }
 
   SECTION ("Negative Numbers (Checking for throw).") {
@@ -68,13 +68,13 @@ TEST_CASE("Comma Delimited Numbers") {
     REQUIRE(stringSum("9,8") == 17);
     REQUIRE(stringSum("5,5") == 10);
     REQUIRE(stringSum("125,54") == 125+54);
-    REQUIRE(stringSum("123445,55314") == 123445+55314);
-    REQUIRE(stringSum("999456,6341") == 999456+6341);
+    REQUIRE(stringSum("123445,55314") == 0+0);
+    REQUIRE(stringSum("999456,6341") == 0+0);
   }
   SECTION ("Multiple Numbers") {
     REQUIRE(stringSum("1,1,1") == 3);
     REQUIRE(stringSum("9,8,241") == 9 + 8 + 241);
-    REQUIRE(stringSum("2313, 5113, 4621, 6621,    431,2") == 2313 + 5113 + 4621 + 6621 + 431 + 2);
+    REQUIRE(stringSum("2313, 5113, 4621, 6621,    431,2") == 0 + 0 + 0 + 0 + 431 + 2);
     REQUIRE(stringSum("2, 523,  5") == 2 + 523 + 5);
   }
 }
@@ -83,8 +83,8 @@ TEST_CASE("Newline Delimited Numbers") {
   SECTION ("Multiple Numbers") {
     REQUIRE(stringSum("1\n1") == 2);
     REQUIRE(stringSum("1\n0") == 1);
-    REQUIRE(stringSum("131\n4141\n0\n896") == 131 + 4141 + 0 + 896);
-    REQUIRE(stringSum("16211\n415141\n10\n896\n4") == 16211 + 415141 + 10 + 896 + 4);
+    REQUIRE(stringSum("131\n4141\n0\n896") == 131 + 0 + 0 + 896);
+    REQUIRE(stringSum("16211\n415141\n10\n896\n4") == 0 + 0 + 10 + 896 + 4);
   }
 }
 
@@ -93,5 +93,35 @@ TEST_CASE("Newline and Comma Delimited Numbers") {
     REQUIRE(stringSum("1\n0,3") == 4);
     REQUIRE(stringSum("51\n31,9") == 51 + 31 + 9);
     REQUIRE(stringSum("124,41, 54\n78\n2") == 124 + 41 + 54 + 78 + 2);
+  }
+}
+
+TEST_CASE("Custom Delimiters") {
+  SECTION ("Single Letter Delimiter") {
+    REQUIRE (stringSum("//1\n123213") == 232 + 3);
+    REQUIRE (stringSum("// t\n25t213") == 25 + 213);
+    REQUIRE (stringSum("//9\n9999999") == 0);
+    REQUIRE (stringSum("// 2\n23") == 3);
+  }
+
+  SECTION ("Muliple Letter Delimiter") {
+    REQUIRE (stringSum("// [test]\n123test123test456") == 123 + 123 + 456);
+    REQUIRE (stringSum("//[123]\n9912388812377771231") == 99 + 888 + 0 + 1);
+    REQUIRE (stringSum("//   [987]\n9987898779876") == 9 + 8 + 7 + 6);
+  }
+
+  SECTION ("Multiple Single Letter Delimiters") {
+    REQUIRE (stringSum("// 1 a b c 9 \n 213a4b5c697") == 2 + 3 + 4 + 5 + 6 + 7);
+    REQUIRE (stringSum("//t a b \n 1t20a30b431") == 1 + 20 + 30 + 431);
+  }
+
+  SECTION ("Multiple Multi-Letter Delimiters") {
+    REQUIRE (stringSum("// [abc][123] [1 2]\n 4abc512361 27") == 4 + 5 + 6 + 7);
+    REQUIRE (stringSum("//[] [ab] [qw3rty]\n 123ab456qw3rty789") == 123 + 456 + 789);
+  }
+
+  SECTION ("Both Single and Multi-Letter Delimiters") {
+    REQUIRE (stringSum("//[abc] a 2 [12]\n3abc4a526127a1") == 3 + 4 + 5 + 6 + 7 + 1);
+    REQUIRE (stringSum("// 1 [,,][__] n\n3n4n516,,7__8") == 3 + 4 + 5 + 6 + 7 + 8);
   }
 }
